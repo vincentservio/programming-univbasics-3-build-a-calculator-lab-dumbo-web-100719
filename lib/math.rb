94c9035feffd4f915671679e191d7042fac412fa
vincent servio <vincentservio@gmail.com>
2019-08-30 21:06:47 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,34 @@
 def addition(num1, num2)
-
+ sum = num1 + num2
+  p sum
 end
 
 def subtraction(num1, num2)
-
+  difference = num1 - num2
+  p difference
 end
 
 def division(num1, num2)
+  quotient = num1 / num2
+  p quotient
 
 end
 
 def multiplication(num1, num2)
+  product = num1 * num2
+  p product
 
 end
 
 def modulo(num1, num2)
+  remainder = num1 % num2
+  p remainder
 
 end
 
+
 def square_root(num)
+  p Math.sqrt(num)
+  
 
 end
